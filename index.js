fdc6d196361394bde26868e741684f68e41920ca
Jihang Li <lijihang26@gmail.com>
2022-04-30 19:59:03 -0400

    Fix data type

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ export async function run() {
       formData.append('repoName', repoName);
       formData.append('tag', tag);
       formData.append('taskName', taskName);
-      formData.append('isPrivate', isPrivate);
+      formData.append('isPrivate', isPrivate.toString());
       formData.append('description', '');
       formData.append('password', password);
       formData.append('commit', commit);
